9f7ab2e909b8eed15305f9213130b4a4c2010888
wai boon <wongweibin99@gmail.com>
2022-02-25 22:06:14 +0800

    added bootstrap

--- src/MainPage/SearchBar.js
+++ src/MainPage/SearchBar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      <p>Search Here: </p>
+      <Button>Search</Button>
+
+    </div>
+  );
 }
 
 export default SearchBar;
